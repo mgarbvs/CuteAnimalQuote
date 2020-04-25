9dfe0ecf12dd25163164dccc2358266376fc7bd7
mgarbvs <michael.garbus@gmail.com>
2020-04-25 16:53:29 -0700

    Starting updates and ideas

--- app/src/main/java/com/example/cuteanimalquote/MainActivity.java
+++ app/src/main/java/com/example/cuteanimalquote/MainActivity.java
@@ -9,7 +9,11 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //the plan: cute animal and a meme quote maybe? And then have it read aloud?
-    // Perhaps we could have some nice music playing in the background too.
+    // Perhaps we could have some nice music playing in the background too. https://developers.deezer.com/login?redirect=/api
+    //Options: https://random.dog/, https://aws.random.cat/view/89,
+    //Other ideas: Movie reccomender
+    //Random book from libgen http://garbage.world/posts/libgen/
+
 
 
     @Override
