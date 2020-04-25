d1ca240e464de69be93ae7ebd99f62fbd0293678
mgarbvs <michael.garbus@gmail.com>
2020-04-25 16:49:39 -0700

    Starting updates

--- app/src/main/java/com/example/cuteanimalquote/MainActivity.java
+++ app/src/main/java/com/example/cuteanimalquote/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.cuteanimalquote;
+import android.content.Context;
+// import com.android.volley.*; need to add volley to out library
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    //the plan: cute animal and a meme quote maybe? And then have it read aloud?
+    // Perhaps we could have some nice music playing in the background too.
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
