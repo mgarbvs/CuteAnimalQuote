00fb1acbcb1b10c562e6a8fd5cf9009bc6485238
mgarbvs <michael.garbus@gmail.com>
2020-05-01 18:59:39 -0700

    Lets hope this works

--- app/src/main/java/com/example/cuteanimalquote/MainActivity.java
+++ app/src/main/java/com/example/cuteanimalquote/MainActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+//mgarbus2 ericxu2
 public class MainActivity extends AppCompatActivity {
     ImageView animalPicture;
     TextView quote;
@@ -27,7 +27,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        taphere = findViewById(R.id.tapHerePleaseWork); // why doesnt this work?????????????????????????????/
+        taphere = findViewById(R.id.tapHerePleaseWork); // why doesnt this work?????????????????????????????/ it works now???
+        // we need a quote box
         instructions = findViewById(R.id.instruction);
         animalPicture = findViewById(R.id.pictureAnimal);
 
