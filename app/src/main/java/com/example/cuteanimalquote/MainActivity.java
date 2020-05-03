f6cfb6b88db7dcb58d0f215ed81707928ec47b99
mgarbvs <michael.garbus@gmail.com>
2020-05-03 15:46:09 -0700

    Updated code, quote ideas, music

--- app/src/main/java/com/example/cuteanimalquote/MainActivity.java
+++ app/src/main/java/com/example/cuteanimalquote/MainActivity.java
@@ -18,10 +18,8 @@ public class MainActivity extends AppCompatActivity {
     //the plan: cute animal and a meme quote maybe? And then have it read aloud?
     // Perhaps we could have some nice music playing in the background too. https://developers.deezer.com/login?redirect=/api
     //quotes: https://fortunecookie.docs.apiary.io/#reference/cookie/list-all-fortunes?console=1
-    //Options: https://random.dog/, https://aws.random.cat/view/89,
-    //Other ideas: Movie reccomender
-    //Random book from libgen http://garbage.world/posts/libgen/
-    //For background music: https://www.youtube.com/watch?v=p2ffzsCqrs8
+    //For background music: https://www.tutorialspoint.com/how-to-play-background-music-in-android-app
+    //
 
 
 
@@ -34,5 +32,12 @@ public class MainActivity extends AppCompatActivity {
         instructions = findViewById(R.id.instruction);
         animalPicture = findViewById(R.id.pictureAnimal);
 
+    }
+
+    public void runApi() {
+        //http://fortunecookieapi.herokuapp.com/#fortunes-fortune-get for the fortune
+        //Perhaps we can store an array of past fortunes and skip ones we have gotten before? Maybe too ambitions
+        //for dogs: https://api.thedogapi.com/v1/images/search seems to already do the trick, we just need to visit it each time we want a new dog.
+        //background music:
     }
 }
