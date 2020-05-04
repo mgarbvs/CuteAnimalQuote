9ce983cc60b23264da7ae3f10c30953a3bd70e91
mgarbvs <michael.garbus@gmail.com>
2020-05-04 00:14:07 -0700

    Everythings working!!!!

--- app/src/main/java/com/example/cuteanimalquote/MainActivity.java
+++ app/src/main/java/com/example/cuteanimalquote/MainActivity.java
@@ -1,22 +1,23 @@
 package com.example.cuteanimalquote;
-import android.content.Context;
+
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+
 import com.squareup.picasso.Picasso;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import com.android.volley.*; //lets just get everything
+import com.android.volley.*;
 import com.android.volley.toolbox.*;
 
-import org.json.*; // for later;
+import org.json.*;
 //mgarbus2 ericxu2
 public class MainActivity extends AppCompatActivity {
     ImageView animalPicture;
     TextView quote;
     TextView instructions;
+    int initialNumber;
     private String messageToShow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,24 +28,43 @@ public class MainActivity extends AppCompatActivity {
         quote = findViewById(R.id.FortuneText);
         quote.setText("");
         Button taphere = findViewById(R.id.tapHerePleaseWork);
+        initialNumber = 0;
         taphere.setOnClickListener(unused -> {
             runApi();
         });
     }
     void runApi() {
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://api.thedogapi.com/v1/images/search";
-        //String url = "https://dog.ceo/api/breeds/image/random";
-        String apiKey = "06bd0cd7-415f-45f3-848c-0919a7b195af";
+        //String url = "https://api.thedogapi.com/v1/images/search";
+        String url;
+        boolean dog;
+        String apiCall;
+        if (initialNumber % 2 == 0) {
+            url = "https://dog.ceo/api/breeds/image/random";
+            dog = true;
+            apiCall = "message";
+            initialNumber = -1;
+        } else {
+            url = "https://randomfox.ca/floof/?ref=apilist.fun";
+            dog = false;
+            apiCall = "image";
+            initialNumber++;
+        }
+        //https://randomfox.ca/floof/?ref=apilist.fun
+        //String apiKey = "06bd0cd7-415f-45f3-848c-0919a7b195af";
         try {
-            new Header("x-api-key", apiKey);
+            //new Header("x-api-key", apiKey);
             JsonObjectRequest dogRequestAPI = new JsonObjectRequest(Request.Method.GET, url, null,
-                    new Response.Listener<JSONArray>() {
+                    new Response.Listener<JSONObject>() {
                         @Override
-                        public void onResponse(JSONArray response) {
+                        public void onResponse(JSONObject response) {
                             Log.d("program", "working");
                             try {
-                                JSONObject id = response.getJSONObject(response.length() - 2);
+                                //JSONObject id = response
+                                if (dog == false) {
+
+                                }
+                                String newUrl = response.get(apiCall).toString();
                                 Picasso.get().load(newUrl).resize(600,500).centerCrop().into(animalPicture);
                                 Log.d("program", "working");
                             } catch (JSONException e) {
@@ -62,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             System.out.println("Header/API key error? IDK");
         }
-        /**try {
+        try {
             // Here goes fortune code
             // http://yerkee.com/api
             String fortuneUrl = "http://yerkee.com/api/fortune/";
@@ -71,8 +91,8 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void onResponse(JSONObject response) {
                             try {
-                                JSONObject fortune = response.getJSONObject("fortune");
-                                String message = fortune.getString("fortune");
+                                //JSONObject fortune = response.getJSONObject("fortune");
+                                String message = response.get("fortune").toString();
                                 System.out.println(message);
                                 messageToShow = message;
                                 quote.setText(message);
@@ -80,10 +100,9 @@ public class MainActivity extends AppCompatActivity {
                                 Log.d("program", "working");
                                 // where am i supposed to display the message of the fortune??
 
-
                             } catch (JSONException e) {
                                 Log.e("program", "json exception");
-                                System.out.println("oops we got an exception");
+                                System.out.println("JSON Exception with the quote!" + e.toString());
                             }
 
                         }
@@ -94,8 +113,7 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
             });
-            requestQueue.add(fortuneRequest);
-
+            queue.add(fortuneRequest);
             //I don't know what this thing does^
             //Do you know how to display the message after you get the request from the API?
 
@@ -106,6 +124,5 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             System.out.print("Whoa, this is a big error");
         }
-         **/
     }
 }
